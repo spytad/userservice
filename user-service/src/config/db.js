7eb7ca565fce4267ef957a4a1bb54d04cb7a96a6
spytad <spydertad@gmail.com>
2025-06-24 16:23:18 -0400

    save before exit

--- user-service/src/config/db.js
+++ user-service/src/config/db.js
@@ -1,9 +1,13 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
+const dotenv = require("dotenv");
 dotenv.config();
+const mongoose = require("mongoose");
+
+// ✅ Add this debug log here
+console.log('MONGO_URI:', process.env.MONGO_URI);
 
 const connectDB = async () => {
+
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
